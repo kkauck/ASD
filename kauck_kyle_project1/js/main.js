9aa25cc48e51f19b6152909ff812b5a884456395
g78624 <kyle0nlin3@fullsail.edu>
2013-07-11 14:39:08 -0600

    Added basic setup for my JS
    July 11, 2013

--- kauck_kyle_project1/js/main.js
+++ kauck_kyle_project1/js/main.js
@@ -3,3 +3,15 @@
  *ASD 1307
  *Warcraft Character Tracker Javascript
 */
+
+$("#index").on("pageinit", function(){
+    //...
+});
+
+$("#addCharacter").on("pageinit", function() {
+    //...
+});
+
+$("#display").on("pageinit", function () {
+   //... 
+});
