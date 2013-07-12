40b765bae6baf332d6816a0778a28a16cba93b68
g78624 <kyle0nlin3@fullsail.edu>
2013-07-12 15:40:47 -0600

    Add my Week 2 Repo
    July 12, 2013

---
+++ kauck_kyle_project2/js/main.js
@@ -0,0 +1,17 @@
+/*
+ *Kyle Kauck
+ *ASD 1307
+ *Warcraft Character Tracker Javascript
+*/
+
+$("#index").on("pageinit", function(){
+    //...
+});
+
+$("#addCharacter").on("pageinit", function() {
+    //...
+});
+
+$("#display").on("pageinit", function () {
+   //... 
+});
