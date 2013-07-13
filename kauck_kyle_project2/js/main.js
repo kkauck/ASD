4b695eafbcba7215f98326cd2910881e9a356e45
g78624 <kyle0nlin3@fullsail.edu>
2013-07-12 19:52:21 -0600

    Working on Form Validation and Saving
    July 12, 2013

--- kauck_kyle_project2/js/main.js
+++ kauck_kyle_project2/js/main.js
@@ -9,7 +9,49 @@ $("#index").on("pageinit", function(){
 });
 
 $("#addCharacter").on("pageinit", function() {
-    //...
+    
+    $("#characterAdd").validate({
+        invalidHandler: function(form, validator) {
+            $("#errors").empty();
+            for (var errorKey in validator.submitted){
+                var errorLabel = $('label[for^="'+ errorKey +'"]').not('.error');
+                $("#errors").append("<span>" + errorLabel.text() +"</span>");
+            };
+        },
+		
+        submitHandler: function() {
+            var data = $("#characterAdd").serializeArray();
+            storeToon(data);
+            window.location.reload();
+        }
+    
+    });
+    
+    storeToon = function (data, toonKey) {
+        if (!toonKey) {
+            var idGenerator = Math.floor(Math.random()*1000000000);
+        } else {
+            idGenerator = toonKey;    
+        }
+                
+        var idGenerator = Math.floor(Math.random()*1000000000);
+        var toonLibrary = {
+            characterName  :[$("#characterName").val()],
+            serverName     :[$("#serverName").val()],
+            race           :[$("#race").val()],
+            class          :[$("#class").val()],
+            roll           :[$("#role").val()],
+            specialization :[$("#specialization").val()],
+            level          :[$("#level").val()],
+            itemLevel      :[$("#itemLevel").val()],
+            professions    :[$("#professions")],
+            extraInfo      :[$("#extraInfo")]
+        };
+        localStorage.setItem(idGenerator, JSON.stringify(toonLibrary));
+        alert("Game Information Has Been Saved!");
+        
+    };
+
 });
 
 $("#display").on("pageinit", function () {
